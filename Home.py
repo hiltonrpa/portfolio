5cb6fc1b56450e79bd5488f0eb9625fcd2062d80
Hilton Albuquerque <hiltonrpa@hotmail.com>
2025-06-21 13:42:07 -0300

    Ajustes diários

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Meu Portfólio",
+    page_icon="🏠",
+    layout="wide"
+)
+st.sidebar.success("Bem-vindo ao Meu Portfólio!")
+st.header("Bem-vindo ao Meu Portfólio!")
+st.write("Use a barra lateral para navegar entre as seções.")
+st.write("TEste git commit.")
---
+++ pages/1_⚽_Estatísticas_do_Brasileirão.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Brasileirão - Estatísticas", 
+    page_icon="📈",
+    layout="wide"
+)
+
+st.header("Dashboard Brasileirão")
+st.write("Este Dashboard Apresenta dados estatisticos do Brasileirão")
+
